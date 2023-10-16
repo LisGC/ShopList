7ee956cd2dece730e5cf3effa18ba09fec0ae16d
LisGC <65579390+LisGC@users.noreply.github.com>
2023-10-16 09:29:19 -0300

    agregar dos clases para añadir items

---
+++ app/src/main/java/cl/lisgc/shoplist/AddBuyItem.kt
@@ -0,0 +1,11 @@
+package cl.lisgc.shoplist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBuyItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_buy_item)
+    }
+}
